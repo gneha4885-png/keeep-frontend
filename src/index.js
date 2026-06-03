d478b6481cbc03056f02f1aafaa6570869dc7f85
Neha Gupta <gneha4885@gmail.com>
2026-06-03 11:03:58 +0530

    Day 17: PWA setup - manifest, service worker, iOS/Android support

--- src/index.js
+++ src/index.js
@@ -15,3 +15,12 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// Register service worker for PWA
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => console.log('SW registered:', reg))
+      .catch(err => console.log('SW error:', err));
+  });
+}
